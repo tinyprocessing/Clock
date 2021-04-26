ac764f0735a75eb400be333cf8c6aa894ff4109d
Mikhail Safir <admin@yelm.io>
2021-04-26 12:21:27 +0300

    Initial Commit

---
+++ Clock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clock
+//
+//  Created by Michael Safir on 26.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
